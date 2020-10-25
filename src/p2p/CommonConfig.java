6890b9f9a1cf10e1fd8321630d745d93af2b1b26
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-25 17:54:36 -0400

    Handling of Load Configuration and PeerInfo File

---
+++ src/p2p/CommonConfig.java
@@ -0,0 +1,60 @@
+package p2p;
+
+public class CommonConfig {
+    int numberOfPreferredNeighbors;
+    int unchokingInterval;
+    int optimisticUnchokingInterval;
+    String fileName;
+    int fileSize;
+    int pieceSize;
+
+    public int getNumberOfPreferredNeighbors() {
+        return numberOfPreferredNeighbors;
+    }
+
+    public void setNumberOfPreferredNeighbors(int numberOfPreferredNeighbors) {
+        this.numberOfPreferredNeighbors = numberOfPreferredNeighbors;
+    }
+
+    public int getUnchokingInterval() {
+        return unchokingInterval;
+    }
+
+    public void setUnchokingInterval(int unchokingInterval) {
+        this.unchokingInterval = unchokingInterval;
+    }
+
+    public int getOptimisticUnchokingInterval() {
+        return optimisticUnchokingInterval;
+    }
+
+    public void setOptimisticUnchokingInterval(int optimisticUnchokingInterval) {
+        this.optimisticUnchokingInterval = optimisticUnchokingInterval;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getPieceSize() {
+        return pieceSize;
+    }
+
+    public void setPieceSize(int pieceSize) {
+        this.pieceSize = pieceSize;
+    }
+
+
+}
