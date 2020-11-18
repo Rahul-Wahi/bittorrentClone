11d91032daa6958184e7865cb1d2b5d547e009c9
Rahul Wahi <rahulwahi42@gmail.com>
2020-11-18 01:25:46 -0500

    Adding Bitfield info in Peer class

--- src/p2p/Peer.java
+++ src/p2p/Peer.java
@@ -1,5 +1,6 @@
 package p2p;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -9,14 +10,43 @@ public class Peer {
     private int portno;
     private boolean hasFile;
     private Set<Integer> neighbours;
+    private int numberOfPieces;
+    private BitField bitField;
+    private Map<Integer, BitField> peersBitField;
+    private Set<Integer> interestedPeers;
 
     public Peer (int peerid, String hostName, int portno, boolean hasFile) {
         this.peerid = peerid;
         this.hostName = hostName;
         this.portno = portno;
         this.hasFile = hasFile;
+        bitField = new BitField(hasFile);
+        peersBitField = new HashMap<>();
     }
 
+    public void addBitField(int peerid, BitField bitField) {
+        peersBitField.put(peerid, bitField);
+    }
+
+    public Map<Integer, BitField> getPeersBitField() {
+        return peersBitField;
+    }
+
+    public BitField getPeerBitField(int remotePeerid) {
+        return peersBitField.get(remotePeerid);
+    }
+
+    synchronized public void addPeersBitField(int remotePeerid, BitField peerBitField) {
+         peersBitField.put(remotePeerid, peerBitField);
+    }
+
+    synchronized public void addInterestedPeers(int remotePeerid) {
+        interestedPeers.add(remotePeerid);
+    }
+
+    synchronized public void removeInterestedPeers(int remotePeerid) {
+        interestedPeers.remove(remotePeerid);
+    }
     public int getPeerid() {
         return peerid;
     }
@@ -63,5 +93,9 @@ public class Peer {
 
     public void selectOptimisticUnchokedNeighbor() {
 
+    }
+
+    public BitField getBitField() {
+        return bitField;
     }
 }
