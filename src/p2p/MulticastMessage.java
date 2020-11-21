8bf2fcadfdf27c59abe69d2d3f65b8ee3020f54b
Rahul Wahi <rahulwahi42@gmail.com>
2020-11-21 04:47:52 -0500

    Rename to MulticastMessage

--- src/p2p/MulticastMessage.java
+++ src/p2p/MulticastMessage.java
@@ -1,23 +1,33 @@
 package p2p;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class BroadcastMessage extends Thread {
+public class MulticastMessage extends Thread {
     private byte[] message;
+    Set<Integer> nodes;
 
-    public BroadcastMessage(byte[] message) {
+    //for all the connections
+    public MulticastMessage(byte[] message) {
         this.message = message;
+        nodes = peerProcess.getCurrentPeer().getConnections().keySet();
+    }
+
+    //only to provided nodes
+    public MulticastMessage(byte[] message, Set<Integer> nodes) {
+        this.message = message;
+        this.nodes = nodes;
     }
 
     public void run() {
         Map<Integer, PeerHandler> connections = peerProcess.getCurrentPeer().getConnections();
         int numberOfCores = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(numberOfCores);
-        for (Integer peeid : connections.keySet()) {
-            executor.submit(() -> connections.get(peeid).sendMessage(message));
+        for (Integer peerid : nodes) {
+            executor.submit(() -> connections.get(peerid).sendMessage(message));
         }
 
         executor.shutdown();
